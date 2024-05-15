6f8c8b3212f6b52b7b4ff24c3b47245f7d0a7536
Marina Kononova <marinakononova@mail.ru>
2024-05-15 18:40:42 +0400

    testNG correctd

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,86 +1,86 @@
 package ru.netology.service;
 
-//import org.junit.jupiter.api.Test;
-
-import org.junit.jupiter.api.Assertions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
     @Test
-    void ShouldShow400IfAmount1600() {
+    public void ShouldShow400IfAmount1600() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1600;
 
         int actual = service.remain(amount);
         int expected = 400;
 
-        Assertions.assertEquals(actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
-    void ShouldShow300IfAmount700() {
+    public void ShouldShow300IfAmount700() {
         CashbackHackService service = new CashbackHackService();
         int amount = 700;
 
         int actual = service.remain(amount);
         int expected = 300;
 
-        Assertions.assertEquals(actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
-    void ShouldShow1IfAmount999() {
+    public void ShouldShow1IfAmount999() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
 
         int actual = service.remain(amount);
         int expected = 1;
 
-        Assertions.assertEquals(actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
-    void ShouldShow999IfAmount1() {
+    public void ShouldShow999IfAmount1() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1;
 
         int actual = service.remain(amount);
         int expected = 999;
 
-        Assertions.assertEquals(actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
-    void ShouldNotShowRemainIfAmount0() {
+    public void ShouldNotShowRemainIfAmount0() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
 
         int actual = service.remain(amount);
         int expected = 0;
 
-        Assertions.assertEquals(actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
-    void ShouldNotShowRemainIfAmount1000() {
+    public void ShouldNotShowRemainIfAmount1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
 
         int actual = service.remain(amount);
         int expected = 0;
 
-        Assertions.assertEquals(actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
-    void ShouldNotShowRemainIfAmountNegative() {
+    public void ShouldNotShowRemainIfAmountNegative() {
         CashbackHackService service = new CashbackHackService();
         int amount = -10;
 
         int actual = service.remain(amount);
         int expected = 0;
 
-        Assertions.assertEquals(actual, expected);
+        assertEquals(actual, expected);
     }
 }
